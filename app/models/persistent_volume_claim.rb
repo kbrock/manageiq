d1d6e029a839b98aa6775da98050e28eba99b727
Keenan Brock <keenan@thebrocks.net>
2023-06-21 11:03:05 -0400

    define PersistentVolumeClaim#persistent_volume as a relation
    define the relation and clean up some rubocops

--- app/models/persistent_volume_claim.rb
+++ app/models/persistent_volume_claim.rb
@@ -1,16 +1,14 @@
 class PersistentVolumeClaim < ApplicationRecord
-  belongs_to :ext_management_system, :foreign_key => "ems_id"
+  # technically this belongs_to :class_name => "ManageIQ::Providers::ContainerManager"
+  belongs_to :ext_management_system, :foreign_key => "ems_id", :inverse_of => :persistent_volume_claims
   belongs_to :container_project
-  has_many :container_volumes
+  has_many :container_volumes, :inverse_of => :persistent_volume_claim # rubocop:disable Rails/HasManyOrHasOneDependent
+  has_one :persistent_volume, -> { where(:type => 'PersistentVolume') }, :class_name => "ContainerVolume", :inverse_of => :persistent_volume_claim # rubocop:disable Rails/HasManyOrHasOneDependent
   serialize :capacity, Hash
   serialize :requests, Hash
   serialize :limits, Hash
 
   virtual_column :storage_capacity, :type => :integer
-
-  def persistent_volume
-    container_volumes.find_by_type('PersistentVolume')
-  end
 
   def storage_capacity
     capacity[:storage]
--- spec/models/persistent_volume_claim_spec.rb
+++ spec/models/persistent_volume_claim_spec.rb
@@ -18,4 +18,19 @@ RSpec.describe PersistentVolumeClaim do
       expect(persistent_volume.storage_capacity).to be nil
     end
   end
+
+  describe "#persistent_volume" do
+    it "finds none" do
+      pvc = FactoryBot.create(:persistent_volume_claim)
+      FactoryBot.create(:container_volume, :persistent_volume_claim => pvc)
+      expect(pvc.persistent_volume).to be_nil
+    end
+
+    it "finds one" do
+      pvc = FactoryBot.create(:persistent_volume_claim)
+      FactoryBot.create(:container_volume, :persistent_volume_claim => pvc)
+      pv = FactoryBot.create(:persistent_volume, :persistent_volume_claim => pvc)
+      expect(pvc.persistent_volume).to eq(pv)
+    end
+  end
 end
